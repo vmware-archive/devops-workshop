fc8f1f26d964ca468b908a0626fb60c2bef69095
Chris Phillipson <cphillipson@pivotal.io>
2017-09-29 08:41:53 -0700

    Complete lab retread to Gradle

---
+++ labs/solutions/06/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
@@ -0,0 +1,18 @@
+package io.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@SpringBootApplication
+public class CloudNativeSpringUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeSpringUiApplication.class, args);
+	}
+}
