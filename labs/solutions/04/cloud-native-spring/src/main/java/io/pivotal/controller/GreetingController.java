db2ccfddde076dbd71b0f730f5d8a90db4622e81
Chris Phillipson <cphillipson@pivotal.io>
2017-10-06 12:39:24 -0700

    These updates close issue #9 * Adding @RefreshScope where appropriate

--- labs/solutions/04/cloud-native-spring/src/main/java/io/pivotal/controller/GreetingController.java
+++ labs/solutions/04/cloud-native-spring/src/main/java/io/pivotal/controller/GreetingController.java
@@ -3,7 +3,9 @@ package io.pivotal.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.scope.refresh.RefreshScope;
 
+@RefreshScope
 @RestController
 public class GreetingController {
 
--- labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
+++ labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -3,11 +3,9 @@ package io.pivotal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.scope.refresh.RefreshScope;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RefreshScope
 public class CloudNativeSpringApplication {
 
 	public static void main(String[] args) {
