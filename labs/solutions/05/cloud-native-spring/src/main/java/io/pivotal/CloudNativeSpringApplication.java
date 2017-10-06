404711afa8280d4a8fbf49531b98377d708a7628
Chris Phillipson <cphillipson@pivotal.io>
2017-10-06 07:58:30 -0700

    Forgot to add @RefreshScope to solution

--- labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
+++ labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -3,9 +3,11 @@ package io.pivotal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.scope.refresh.RefreshScope;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope
 public class CloudNativeSpringApplication {
 
 	public static void main(String[] args) {
