16991bf0f0c59640432b19735afc659081f88945
Chris Phillipson <cphillipson@pivotal.io>
2018-08-23 18:23:13 -0700

    Updates to labs 05 and 06 impl and documentation

--- labs/solutions/06/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
+++ labs/solutions/06/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
@@ -3,7 +3,7 @@ package io.pivotal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @EnableDiscoveryClient
