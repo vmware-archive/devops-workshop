c0b0316e9187aad8117a90130e2887ef43ceb6df
Chris Phillipson <cphillipson@pivotal.io>
2018-10-22 17:03:45 -0700

    Spring Boot 2.0 and Cloud Finchley Retread (#24)
    * First part of larger effort to upgrade devops-workshop-java
    * Labs docs and solutions moving to Spring Boot 2 and Spring Cloud Finchley
    * This check-in completes retread of README and labs docs and solutions up to lab 04
    * Still need to complete labs 05 and 06.  Plus we'll need to go back and retread the slide decks.  And finally reproduce the PDF.
    
    * Figured out how to conditionally disable security
    * so we can run lab 04 locally before deploying to Cloud Foundry
    
    * Lab 05 solution impl and documentation updated
    
    * Updates to labs 05 and 06 impl and documentation
    
    * First pass at PDF generation
    * Need to update UI screenshot from more recent release of PAS 2..x AppsManager
    
    * Add Travis-CI integration
    
    * Add Travis-CI build badge
    
    * Update Travis-CI script
    
    * Update Travis-CI script
    * Change directories before executing Gradle tasks
    
    * Fix imports in lab 05 solution
    
    * Fix ./gradlew in .travis.yml
    
    * Update Docker and Vagrant images
    
    * Major updates
    * Support for building, packaging and running examples w/ OpenJDK 11
    * Update wrapper to Gradle 4.10.2
    * Add JavaAssist 3.24.0-rc lib as a stopgap until GA release; in support of OpenJDK 11
    * Upgrade to Spring Boot 2.0.6.RELEASE
    * Upgrade other third-party dependencies and plugins
    
    * Two options to connect to and/or administer the mysql database
    
    * Move manifest.yml into the right place
    
    * Update Travis-CI to support building with Oracle and OpenJDK 8/11 variants
    
    * Update Gradle wrapper to 4.10.2
    
    * Update slide decks, sample schedule, and pdf version of workshop guide

--- labs/solutions/04/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
+++ labs/solutions/04/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -3,10 +3,28 @@ package io.pivotal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
 @SpringBootApplication
 public class CloudNativeSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudNativeSpringApplication.class, args);
+	}
+
+	@Profile("!cloud")
+	@Configuration
+	static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    	@Override
+    	public void configure(WebSecurity web) throws Exception {
+        	web
+           		.ignoring()
+               	.antMatchers("/**");
+    	}
 	}
 }
--- labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
+++ labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -2,6 +2,13 @@ package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
@@ -10,5 +17,17 @@ public class CloudNativeSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudNativeSpringApplication.class, args);
+	}
+
+	@Profile("!cloud")
+	@Configuration
+	static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    	@Override
+    	public void configure(WebSecurity web) throws Exception {
+        	web
+           		.ignoring()
+               	.antMatchers("/**");
+    	}
 	}
 }
