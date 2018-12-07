a77e69c72d9a846f1205d79bfa8e7c32a6c40502
Chris Phillipson <cphillipson@pivotal.io>
2018-12-07 05:57:03 -0800

    More refinements in support of Spring Boot 2.1.1.RELEASE and Spring Cloud Greenwich.M3 * Standardize on cflinuxfs3 stack * Add optional manifest.jdk11.yml for deploying OpenJDK 11 compiled code * Updates to documentation for clarity * Get lab05 and lab06 working; had to drop back to Spring Boot 2.0.7.RELEASE and Spring Cloud Finchley.SR2 for UI * Updated repositories and dependencies across build.gradle files

--- labs/solutions/06/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
+++ labs/solutions/06/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
@@ -2,12 +2,12 @@ package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -29,4 +29,5 @@ public class CloudNativeSpringUiApplication {
                		.antMatchers("/**");
     	}
 	}
+
 }
