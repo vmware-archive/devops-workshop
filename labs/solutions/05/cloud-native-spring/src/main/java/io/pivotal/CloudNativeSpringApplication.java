ad367c5c946f3be9f4fd6afa78543d68f3805970
Chris Phillipson <cphillipson@pivotal.io>
2019-01-26 11:22:45 -0800

    Upgrade to Spring Boot 2.1.2.RELEASE and Spring Cloud Greenwich.RELEASE * update build.gradle dependencies -> occurrences of compile updated to implementation, third-party dependencies updated to latest available versions, lombok updated to work with openjdk 11 * lab instructions and build.gradle updated to accommodate more permissive spring-security-config during local workstation deployments from labs/solutions/04/... onward * pdf version of lab guide updated

--- labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
+++ labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -1,10 +1,12 @@
 package io.pivotal;
 
+import org.springframework.context.annotation.Profile;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -18,14 +20,13 @@ public class CloudNativeSpringApplication {
 		SpringApplication.run(CloudNativeSpringApplication.class, args);
 	}
 
+	@Profile("!cloud")
 	@Configuration
 	static class ApplicationSecurityOverride extends WebSecurityConfigurerAdapter {
 
     	@Override
-    	public void configure(WebSecurity web) throws Exception {
-        	web
-           		.ignoring()
-               		.antMatchers("/**");
+    	public void configure(HttpSecurity web) throws Exception {
+			web.authorizeRequests().antMatchers("/**").permitAll();
     	}
 	}
 
--- labs/solutions/06/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
+++ labs/solutions/06/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
@@ -1,12 +1,14 @@
 package io.pivotal;
 
+import org.springframework.context.annotation.Profile;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableDiscoveryClient
@@ -19,14 +21,13 @@ public class CloudNativeSpringUiApplication {
 		SpringApplication.run(CloudNativeSpringUiApplication.class, args);
 	}
 
+	@Profile("!cloud")
 	@Configuration
 	static class ApplicationSecurityOverride extends WebSecurityConfigurerAdapter {
 
     	@Override
-    	public void configure(WebSecurity web) throws Exception {
-        	web
-           		.ignoring()
-               		.antMatchers("/**");
+    	public void configure(HttpSecurity web) throws Exception {
+			web.authorizeRequests().antMatchers("/**").permitAll();
     	}
 	}
 
