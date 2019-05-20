b417ca3276bf5407a8da2bdcb2c8c4b7b5fa6577
Chris Phillipson <cphillipson@pivotal.io>
2019-05-19 23:48:15 -0700

    Update convenience script for full deployment of lab 7 solution

--- labs/solutions/07/spring-cloud-gateway/deploy-all.sh
+++ labs/solutions/07/spring-cloud-gateway/deploy-all.sh
@@ -8,15 +8,17 @@ cf push --no-start
 cf bs cloud-native-spring config-server
 cf bs cloud-native-spring service-registry
 cf start cloud-native-spring
-cd ../../06/cloud-native-spring-ui
-cf push --no-start
-./gradlew clean build
-cf bs cloud-native-spring-ui service-registry
-cf bs cloud-native-spring-ui circuit-breaker
-cf start cloud-native-spring-ui
+
 cd ../../07/spring-cloud-gateway
 cf push --no-start
 ./gradlew clean build
 cf bs spring-cloud-gateway config-server
 cf bs spring-cloud-gateway service-registry
 cf start spring-cloud-gateway
+
+cd ../../07/cloud-native-spring-ui
+cf push --no-start
+./gradlew clean build
+cf bs cloud-native-spring-ui service-registry
+cf bs cloud-native-spring-ui circuit-breaker
+cf start cloud-native-spring-ui
